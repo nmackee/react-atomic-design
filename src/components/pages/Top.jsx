1540af5002491af3ca0e6ea5d97d3da6d046b6b8
nmackee <70114875+nmackee@users.noreply.github.com>
2022-01-18 15:23:04 +0900

    Contextでのstate管理（（ユーザー情報の設定と参照)

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,17 +1,26 @@
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
-  const onClickAdmin = () => history.push({ pathname: "/users", state: { isAdmin: true }});
-  const onClickGeneral = () => history.push({ pathname: "/users", state: { isAdmin: false }});
+  const { setUserInfo } = useContext(UserContext);
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
   return (
     <SContainer>
       <h2>TOPページです</h2>
       <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
